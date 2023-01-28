357cc6b69a1a27040606be08d8541f9e1c22796c
Darshana Perera <darshana.p@hsenidmobile.com>
2023-01-28 11:02:43 +0530

    copyright line created

---
+++ src/Design/Components/Copyright.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Copyright() {
+  return (
+    <div className="copyright-line-bottom">
+      <p className="copyright-text">Chat-bot Dashboard powered by ~link will be here~</p>
+    </div>
+  );
+}
